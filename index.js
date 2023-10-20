9e53768a79f7349c2c3ff2520327e88ca1839a83
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-10-20 07:57:40 +0600

    insert product into database

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app=express();
 const port=process.env.PORT || 5000;
 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ojnnavp.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,12 +22,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection=client.db("usersDB").collection("addproducts");
+
+    // add products
+    app.post('/addproducts',async(req,res)=>{
+      const product=req.body;
+      console.log('new product',product);
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    })
+
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+  } 
+  finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
