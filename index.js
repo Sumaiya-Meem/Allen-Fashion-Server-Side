a5f99cff90c96700f709a2341ac5b170a7ae70bb
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-10-20 20:07:27 +0600

    make update product functionality

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express=require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const app=express();
-const port=process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ojnnavp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ojnnavp.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,42 +23,68 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productsCollection=client.db("fashionDB").collection("addproducts");
-    const usersCollection=client.db("fashionDB").collection("users");
+    const productsCollection = client.db("fashionDB").collection("addproducts");
+    const usersCollection = client.db("fashionDB").collection("users");
 
     // add products
-    app.post('/addproducts',async(req,res)=>{
-      const product=req.body;
-      console.log('new product',product);
+    app.post('/addproducts', async (req, res) => {
+      const product = req.body;
+      console.log('new product', product);
       const result = await productsCollection.insertOne(product);
       res.send(result);
     })
     // show products data
-    app.get('/addproducts',async(req,res)=>{
-      const cursor =productsCollection.find()
-      const result=await cursor.toArray();
+    app.get('/addproducts', async (req, res) => {
+      const cursor = productsCollection.find()
+      const result = await cursor.toArray();
       res.send(result);
-  })
+    })
 
-  // update products
-  
+    // load update products
+    app.get('/addproducts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollection.findOne(query)
+      res.send(result);
+    })
+    // update product
+    app.put('/addproducts/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updateProduct = req.body;
+      const product = {
+        $set: {
+          name: updateProduct.name,
+          brandName: updateProduct.brandName,
+          price: updateProduct.price,
+          type: updateProduct.type,
+          rating: updateProduct.rating,
+          image: updateProduct.image
 
-  // add user 
-  app.post('/user',async(req,res)=>{
-    const user=req.body;
-    console.log('new user',user);
-    const result = await usersCollection.insertOne(user);
-    res.send(result);
-  })
+        }
+      }
+      const result = await productsCollection.updateOne(filter, product, options);
+      res.send(result)
+
+    })
+
+    // add user 
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      console.log('new user', user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
 
 
 
 
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
+  }
   finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -71,11 +97,11 @@ run().catch(console.dir);
 app.use(cors());
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send("Simple CRUD is running soon");
+app.get('/', (req, res) => {
+  res.send("Simple CRUD is running soon");
 })
 
 
-app.listen(port,()=>{
-    console.log(`Simple CRUD is Running on Port: ${port}`);
+app.listen(port, () => {
+  console.log(`Simple CRUD is Running on Port: ${port}`);
 })
